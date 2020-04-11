ddc65dfe7ecfdcfc2f7de00801d331d874f134fc
Steven Phan <steven.phan009@gmail.com>
2020-04-11 13:47:46 -0700

    finished divided difference algo

--- lab4_Steven_Phan_014358810/newtonInterEval.m
+++ lab4_Steven_Phan_014358810/newtonInterEval.m
@@ -1,10 +1,5 @@
 function val = newtonInterEval(x,y,xi)
 
-%  fx0 = D(n,n);
-%  for i = n-1:-1:1
-%     fx0 = fx0*(x0-x(i)) + D(i,i);
-%  end
-
     a = newtonIter(x,y); % Get coefficients
     
     lenA = length(a); 
