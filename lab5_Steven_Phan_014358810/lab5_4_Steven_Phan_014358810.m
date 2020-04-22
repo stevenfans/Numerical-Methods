e4788207f5687289e85c96f044838c5fb5b096fd
Steven Phan <steven.phan009@gmail.com>
2020-04-21 21:44:49 -0700

    finished

--- lab5_Steven_Phan_014358810/lab5_4_Steven_Phan_014358810.m
+++ lab5_Steven_Phan_014358810/lab5_4_Steven_Phan_014358810.m
@@ -1,7 +1,8 @@
+% Steven Phan 
 clc; clear; close all; 
 
 syms x; 
- 
+X = sym('x');
 
 fx = 1/(x.^2 +1);
 fdx = diff(fx);             % 1st derivative
@@ -9,56 +10,69 @@ fddx = diff(fdx);         % 2nd derivative
 fdddx = diff(fddx);     % 3rd derivative
 fddddx = diff(fdddx); % 4th derivative
 
-% e = exp(1);
-% fx = x*e^x;
-
 lowL  = -2; 
 highL =  2;
 
 N = [2,3,4,5];
+
+% Get exact integral value
+intF = vpa(int(fx,X,lowL,highL)); 
+fprintf('True Integral Value: %2.6f\n\n', intF); 
+
 
 % Trapezoidal Rule
 valTrap = zeros(1,length(N)); 
 for i = 1:length(N)
     valTrap(i) = tranpezoidalIntegral(fx,lowL,highL,N(i));
 end
-X = sym('x');
 
-% Get maximum value of second derivative function 
-interval = -2:1:2;
-for i = 1:length(interval)
-    asdf(i) = vpa(subs(fddx,X,interval(i)));
+% Calculate Relative True Error
+for i = 1:length(N) 
+    errTrap(i) = abs((intF-valTrap(i))/intF) * 100; 
 end
-[M,idx] = max(asdf);
 
-% calculate the trapezoidal error
-for i = 1:length(N)
-    errTrap = (highL-lowL)^3/((12*N(i))^2)*abs(M);
-end
+disp('TRAPEZOIDAL RULE');
+fprintf('%0s%23s\n', 'Trapezoidal Approx.','Relative True Error');
+A = [valTrap; errTrap]; 
+fprintf('    %2.6f                      %10.6f \n', A);
+fprintf('\n');
 
 
 % 1/3 Simpson Rule
-valSimp = zeros(1,length(N)); 
-
 for i = 1:length(N)
     valSimp(i) = simpsonIntegral(fx,lowL,highL,N(i));
 end
-
-% Get maximum value of fourth derivative function 
-interval = -2:1:2;
-for i = 1:length(interval)
-    asdf(i) = vpa(subs(fddddx,X,interval(i)));
+% remove all the NaN
+test  = find(valSimp=='NaN');
+for i = length(test):-1:1
+    L = test(i);
+    valSimp(L) = [];
 end
-[M,idx] = max(asdf);
 
-% calculate the 1/3 simpson error
-for i = 1:length(N)
-    errSimp(i) = (((highL-lowL)^5)/(180*N(i)^4))*abs(M);
+% Calculat Relative True Error
+for i = 1:length(valSimp)
+    errSimp(i) = abs((intF-valSimp(i))/intF) * 100; 
 end
+
+disp('1/3 SIMPSON RULE');
+fprintf('%0s%23s\n', '1/3 Simpson Approx.','Relative True Error');
+A = [valSimp; errSimp]; 
+fprintf('    %2.6f                      %10.6f \n', A);
+fprintf('\n');
 
 
 % Gaussian Quadrature Rule
-% valGauss = zeros(1,length(N)); 
 for i = 1:length(N)
     valGauss(i) = nGaussIntegral(fx,lowL,highL,N(i));
 end
+
+% Calculate the Relative True Error
+for i = 1:length(N)
+    errGauss(i) = abs((intF-valGauss(i))/intF) * 100; 
+end
+
+disp('GUASS QUADRATURE RULE');
+fprintf('%0s%23s\n', 'Gauss Quad Approx.','Relative True Error');
+A = [valGauss; errGauss]; 
+fprintf('    %2.6f                      %10.6f \n', A);
+fprintf('\n');
